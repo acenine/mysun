1e11dcc8127197a25a685aaee44af9d1124b3e45
Jarrod Powell <jpowell1032@gmail.com>
2018-04-14 17:22:20 -0700

    new data and more conections stylins

--- src/DetailedView.jsx
+++ src/DetailedView.jsx
@@ -23,13 +23,14 @@ class DetailedView extends React.Component {
 
     return (
       <div>
-      <img src={require(`./${this.props.listing.image}.jpg`)} />
-     <div> {this.props.listing.title}</div>
-     <div>Owner: {this.props.listing.owner}</div>
-     <div>{this.props.listing.about}</div>
-     <div>{this.props.listing.raised} raised </div>
-     <div>{`${percentage}% of goal!`}</div>
+    <img src={require(`./${this.props.listing.image}.jpg`)} />
+      <div>{this.props.listing.description}</div>
+      <div>{this.props.listing.host}</div>
+      <div>{this.props.listing.city}</div>
+      <div>Seeds Required: {this.props.listing.seeds_required}</div>
+      <div>Current Seed Count: {this.props.listing.seeds_count}</div>
      <button type="button" onClick={this.handleButtonClick.bind(this)}>Seed this project</button>
+       <p>&nbsp;</p>
      {inputField}
       </div>
       )
